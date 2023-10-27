1ed841c86ba1f52e2dca3638ddcb045fad79125a
Gayatri Sarnobat <gayatrisarnobat@Gayatris-MacBook-Air.local>
2023-10-27 21:50:44 +0530

    feat: social sign in

---
+++ server.js
@@ -0,0 +1,89 @@
+const fs = require('fs');
+const https = require('https');
+const path = require('path');
+const express = require('express');
+const helmet = require('helmet');
+const passport = require('passport');
+const { Strategy } = require('passport-google-oauth20');
+require('dotenv').config();
+
+const PORT = 3000;
+
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
+
+const AUTH_OPTIONS = {
+  callbackURL: '/auth/google/callback',
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+};
+
+const verifyCallback = (accessToken, refreshToken, profile, done) => {
+  console.log('Profile: ', profile);
+  done(null, profile);
+};
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
+const app = express();
+
+app.use(helmet());
+
+app.use(passport.initialize());
+
+const checkLoggedIn = (req, res, next) => {
+  const isLoggedIn = true; // TODO
+  if (!isLoggedIn) {
+    return res.status(401).json({
+      error: 'You must login!',
+    });
+  }
+  next();
+};
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['email'],
+  })
+);
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', {
+    failureRedirect: '/failure',
+    successRedirect: '/',
+    session: false,
+  }),
+  (req, res) => {
+    console.log('Google called us back!');
+  }
+);
+
+app.get('/auth/logout', (req, res) => {});
+
+app.get('/secret', checkLoggedIn, (req, res) => {
+  return res.send('Your personal secret value is 42!');
+});
+
+app.get('/failure', (req, res) => {
+  return res.send('Failed to log in!');
+});
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync('key.pem'),
+      cert: fs.readFileSync('cert.pem'),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
